1653395239f793ffa73de8405bf3821c8eb5c103
mbaxryt2 <mbaxryt2@student.manchester.ac.uk>
2016-07-02 02:22:07 +0100

    html

--- src/com/dashboard/controller/Dashboard.java
+++ src/com/dashboard/controller/Dashboard.java
@@ -7,12 +7,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class Dashboard {
 //	@RequestMapping()
- 
-	@RequestMapping("/welcome")
-	public ModelAndView helloWorld() {
- 
-		String message = "<br><div style='text-align:center;'>"
-				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from Dashboard.java **********</div><br><br>";
-		return new ModelAndView("welcome", "message", message);
-	}
+// 
+//	@RequestMapping("/welcome")
+//	public ModelAndView helloWorld() {
+// 
+//		String message = "<br><div style='text-align:center;'>"
+//				+ "<h3>********** Hello World, Spring MVC Tutorial</h3>This message is coming from Dashboard.java **********</div><br><br>";
+//		return new ModelAndView("welcome", "message", message);
+//	}
 }
